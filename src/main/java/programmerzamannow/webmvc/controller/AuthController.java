b61299334616e0ce1952cb02a2e8fc27b8dec7c9
Hanif Faiz Hidayat <grey.hanif@gmail.com>
2024-03-27 23:42:53 +0700

    response entity

---
+++ src/main/java/programmerzamannow/webmvc/controller/AuthController.java
@@ -0,0 +1,26 @@
+package programmerzamannow.webmvc.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class AuthController {
+
+    @PostMapping(path = "/auth/login", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public ResponseEntity<String> login(
+            @RequestParam(name = "username") String username,
+            @RequestParam(name = "password") String password
+    ){
+        if ("hanif".equals(username) && "rahasia".equals(password)){
+            return ResponseEntity.status(HttpStatus.OK).body("OK");
+        }else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("KO");
+        }
+    }
+}
