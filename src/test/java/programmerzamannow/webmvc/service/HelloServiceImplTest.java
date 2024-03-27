9717c28bdda6a221fbdac90bcdab025198ef456a
Hanif Faiz Hidayat <grey.hanif@gmail.com>
2024-03-27 19:36:10 +0700

    service

---
+++ src/test/java/programmerzamannow/webmvc/service/HelloServiceImplTest.java
@@ -0,0 +1,20 @@
+package programmerzamannow.webmvc.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class HelloServiceImplTest {
+
+    @Autowired
+    private HelloService helloService;
+
+    @Test
+    void helloGuest() {
+        assertEquals("Hello Guest", helloService.hello(null));
+        assertEquals("Hello Hanif", helloService.hello("Hanif"));
+    }
+}
