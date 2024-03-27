4b1911c7dddaaf04ea8bd1ec3f3d108ba2c998b6
Hanif Faiz Hidayat <grey.hanif@gmail.com>
2024-03-27 23:13:26 +0700

    request body + object mapper json

---
+++ src/main/java/programmerzamannow/webmvc/model/HelloResponse.java
@@ -0,0 +1,14 @@
+package programmerzamannow.webmvc.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloResponse {
+
+    private String hello;
+
+}
