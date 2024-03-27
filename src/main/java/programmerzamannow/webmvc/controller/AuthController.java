9a0dca4d6fac026c861ad5d24b3990219ea501e7
Hanif Faiz Hidayat <grey.hanif@gmail.com>
2024-03-28 03:19:45 +0700

    session attribute

--- src/main/java/programmerzamannow/webmvc/controller/AuthController.java
+++ src/main/java/programmerzamannow/webmvc/controller/AuthController.java
@@ -1,12 +1,15 @@
 package programmerzamannow.webmvc.controller;
 
 import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import programmerzamannow.webmvc.model.User;
 
 @Controller
 public class AuthController {
@@ -15,12 +18,17 @@ public class AuthController {
     public ResponseEntity<String> login(
             @RequestParam(name = "username") String username,
             @RequestParam(name = "password") String password,
+            HttpServletRequest servletRequest,
             HttpServletResponse servletResponse
     ){
         if ("hanif".equals(username) && "rahasia".equals(password)){
+            HttpSession session = servletRequest.getSession(true);
+            session.setAttribute("user", new User(username));
+
             Cookie cookie = new Cookie("username",username);
             cookie.setPath("/");
             servletResponse.addCookie(cookie);
+
             return ResponseEntity.status(HttpStatus.OK).body("OK");
         }else {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("KO");
